4cfb5495bbd957eb853fc00581aaa6ddcf7fb6e2
vivelakorea <gyujang95@snu.ac.kr>
2024-01-11 23:24:47 -0700

    parallelization

--- 2txt2dream3d.py
+++ 2txt2dream3d.py
@@ -1,62 +1,78 @@
 import os
 import json
 import math
+import multiprocessing
 
-def txt2dream3d(texture, numSVEs):
+def txt2dream3d(texture, i):
     # texture = 30
     # numSVEs = 200
 
     curfolder = os.getcwd()
     foldername = f'{curfolder}\\{texture}'
 
-    for i in range(numSVEs):
-        
-        grainIDFile = f'{foldername}\\grainID_{i}.txt'
-        orientationsFile_in = f'{foldername}\\orientations_{i}.txt'
-        
+    grainIDFile = f'{foldername}\\grainID_{i}.txt'
+    orientationsFile_in = f'{foldername}\\orientations_{i}.txt'
+    
 
-        # modify orientationsFile_in to not have grainID
-        orientationsFile_out = f'{foldername}\\orientations_{i}_out.txt'
-        
-        f = open(orientationsFile_in, 'r'); lines = f.readlines(); f.close()
+    # modify orientationsFile_in to not have grainID
+    orientationsFile_out = f'{foldername}\\orientations_{i}_out.txt'
+    
+    f = open(orientationsFile_in, 'r'); lines = f.readlines(); f.close()
 
-        f = open(orientationsFile_out, 'w')
-        for line in lines:
-            tmp = list(map(float,(line.split(' '))))
-            grainId, rcomp1, rcomp2, rcomp3, phase = tmp
-            rodrigues4 = math.sqrt(rcomp1**2. + rcomp2**2. + rcomp3**2.)
-            rodrigues1 = -rcomp1/rodrigues4 if rodrigues4 != 0 else 0
-            rodrigues2 = rcomp2/rodrigues4 if rodrigues4 != 0 else 0
-            rodrigues3 = -rcomp3/rodrigues4 if rodrigues4 != 0 else 0
-            f.write(f'{rodrigues1} {rodrigues2} {rodrigues3} {rodrigues4}\n')
-        f.close()
-
-        # read number of grains
-        f = open(orientationsFile_in, 'r'); lines = f.readlines(); f.close()
+    f = open(orientationsFile_out, 'w')
+    for line in lines:
         tmp = list(map(float,(line.split(' '))))
-        n = int(tmp[0])
+        grainId, rcomp1, rcomp2, rcomp3 = tmp[:4]
+        rodrigues4 = math.sqrt(rcomp1**2. + rcomp2**2. + rcomp3**2.)
+        rodrigues1 = -rcomp1/rodrigues4 if rodrigues4 != 0 else 0
+        rodrigues2 = rcomp2/rodrigues4 if rodrigues4 != 0 else 0
+        rodrigues3 = -rcomp3/rodrigues4 if rodrigues4 != 0 else 0
+        f.write(f'{rodrigues1} {rodrigues2} {rodrigues3} {rodrigues4}\n')
+    f.close()
 
-        # modify Dream3D pipeline
-        outputFile = f'{foldername}\\Output_FakeMatl_{i}.dream3d'
+    # read number of grains
+    f = open(orientationsFile_in, 'r'); lines = f.readlines(); f.close()
+    tmp = list(map(float,(line.split(' '))))
+    n = int(tmp[0])
 
-        data = json.load(open(f'{curfolder}\\reread_using_grain_and_ori2.json'))
-        data['00']['InputFile'] = grainIDFile
-        data['03']['TupleDimensions']['Table Data'] = [[n+1]]
-        data['05']['InputFile'] = orientationsFile_out
-        data['15']['OutputFile'] = outputFile
+    # modify Dream3D pipeline
+    outputFile = f'{foldername}\\Output_FakeMatl_{i}.dream3d'
 
-        modifiedPipeline = f"{foldername}\\tmp_{i}.json"
-        with open(modifiedPipeline, "w") as outfile:
-            json.dump(data, outfile)
+    data = json.load(open(f'{curfolder}\\reread_using_grain_and_ori2.json'))
+    data['00']['InputFile'] = grainIDFile
+    data['03']['TupleDimensions']['Table Data'] = [[n+1]]
+    data['05']['InputFile'] = orientationsFile_out
+    data['15']['OutputFile'] = outputFile
 
-        # run the pipeline
-        dream3dEXE = "C:\\Users\\Gyu-Jang Sim\\Desktop\\DREAM3D-6.5.171-Win64\\PipelineRunner.exe"
-        os.system(f'echo | \"{dream3dEXE}\" -p \"{modifiedPipeline}\"')
+    modifiedPipeline = f"{foldername}\\tmp_{i}.json"
+    with open(modifiedPipeline, "w") as outfile:
+        json.dump(data, outfile)
+
+    # run the pipeline
+    dream3dEXE = "C:\\Users\\Gyu-Jang Sim\\Desktop\\DREAM3D-6.5.171-Win64\\PipelineRunner.exe"
+    os.system(f'echo | \"{dream3dEXE}\" -p \"{modifiedPipeline}\"')
 
 
-txt2dream3d(30,200)
-# txt2dream3d(45,200)
-# txt2dream3d(90,200)
-# txt2dream3d(160,100)
-# txt2dream3d(200,4)
-# txt2dream3d(250,2)
+################################################################################################################################################################
+################################################################################################################################################################
+################################################################################################################################################################
+################################################################################################################################################################
+
+def func(i):
+    
+    ############## change here ##############
+    texture = 250
+    #########################################
+
+    txt2dream3d(texture,i)
+
+
+if __name__ == '__main__':
+
+    ############### change here ###############
+    numSVEs = 2
+    ############################################
+    
+    pool_obj = multiprocessing.Pool(61)
+    pool_obj.map(func, list(range(numSVEs)))
+    
